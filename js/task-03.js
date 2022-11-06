8bb63eea98fc7363648140ec83e2dc4ac4ef7e46
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-06 09:43:19 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector(".gallery");
+
+const markup = images
+  .map((image) => `<li class="gallery__item"><img class="gallery__img" src="${image.url}" alt="${image.alt}"></li>`)
+  .join("");
+
+list.insertAdjacentHTML("beforeend", markup);
