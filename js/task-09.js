07a8fc77e0bc20860e4f7a48d005bb44bad0e600
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-06 13:24:33 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector(`.change-color`),
+  span: document.querySelector(`.color`),
+};
+
+refs.button.addEventListener(`click`, onBodyChangeColor);
+
+function onBodyChangeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = document.body.style.backgroundColor; 
+};
