1e58132fafd77a9fee991bc9894c483ab7ef6f86
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-06 12:57:42 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const formEmail = formElements.email.value;
+    const formPassword = formElements.password.value;
+    
+    if (formEmail === "" || formPassword === "") {
+        alert("Please enter the data!");
+    } else {
+        console.log(`Email: ${formEmail}, Password: ${formPassword}`);
+    };
+    
+    event.currentTarget.reset();
+}
