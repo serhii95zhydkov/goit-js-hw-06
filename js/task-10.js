a0a9cfd890bb117b7624f05e6f0c729fe819ef9f
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-06 14:27:24 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  divControls: document.querySelector(`#controls`),
+  input: document.querySelector(`input`),
+  buttonCreate: document.querySelector(`button[data-create]`),
+  buttonDestroy: document.querySelector(`button[data-destroy]`),
+  divBoxes: document.querySelector(`#boxes`),
+}
+
+refs.buttonCreate.addEventListener(`click`, createBoxes);
+refs.buttonDestroy.addEventListener(`click`, destroyBoxes);
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+
+  const arrayDiv = [];
+
+  for (let i = 1; i <= amount; i += 1) {
+    let divBoxesEl = `<div class="box-${i}"></div>`;
+    arrayDiv.push(divBoxesEl);
+  };
+
+  const markup = arrayDiv.join("");
+
+  refs.divBoxes.insertAdjacentHTML("beforeend", markup);
+
+  for (let i = 0; i < amount; i += 1) {
+    refs.divBoxes.children[i].style.width = (30 + 10 * i) + "px";
+    refs.divBoxes.children[i].style.height = (30 + 10 * i) + "px";
+    refs.divBoxes.children[i].style.backgroundColor = getRandomHexColor();
+  }
+};
+
+function destroyBoxes() {
+  refs.divBoxes.innerHTML = "";
+};
